a67ed38367a9337fb50a4595da24544f0af8f9c0
MariaLozano01 <102619474+MariaLozano01@users.noreply.github.com>
2022-11-19 02:03:19 -0500

    updates

--- dash.py
+++ dash.py
@@ -4,57 +4,27 @@ import numpy as np
 
 st.title('OT Project')
 
-DATE_COLUMN = 'time_locs'
-DATA_URL = ('5glhn8oo_clean_data.csv')
-
-
-
-DATE_COLUMN_2 = 'time_locs'
-DATA_URL_2 = ('5h167gur_clean_data.csv')
-
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-
-@st.cache
-def load_data(nrows):
-    data_2 = pd.read_csv(DATA_URL_2, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data_2.rename(lowercase, axis='columns', inplace=True)
-    data_2[DATE_COLUMN_2] = pd.to_datetime(data_2[DATE_COLUMN_2])
-    return data_2
-
-
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
-
-
-data_load_state_2 = st.text('Loading data...')
-data_2 = load_data(10000)
-data_load_state_2.text("Done! (using st.cache)")
+patient_data_1 = pd.read_csv('5glhn8oo_clean_data.csv')
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
-    st.write(data)
+    st.write(patient_data_1)
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data_2)
+################################## PATIENT 2 ##################################
 
-patient_1 = pd.read_csv('5glhn8oo_clean_data.csv')
-patient_2 = pd.read_csv('5h167gur_clean_data.csv')
+patient_data_2 = pd.read_csv('5h167gur_clean_data.csv')
+
+if st.checkbox('Show Raw Data 2'):
+    st.subheader('Raw Data 2')
+    st.write(patient_data_2)
+
+
 st.subheader('Satisfaction Patient 1')
-satis = patient_1['Satisfaction'].value_counts()
+satis = patient_data_1['Satisfaction'].value_counts()
 st.bar_chart(satis)
 
 st.subheader('Types of Activity for Patient 2')
-activity = patient_2[['heavy_activity','mild_activity','moderate_activity']]
+activity = patient_data_2[['heavy_activity','mild_activity','moderate_activity']].value_counts()
 st.line_chart(activity)
 
 code = '''@st.cache
@@ -68,7 +38,35 @@ def load_data(nrows):
 st.code(code,language= 'python')
 st.caption('Above is Code that displays the raw dataset')
 
+
+#sudo apt-get update && sudo apt-get upgrade -->to update and upgrade terminal at the same time
+#sudo apt install python3-pip
 #pip3 install streamlit 
 #pip3 install pipenv or Sudo -H pip install -U pipenv 
 #pipenv shell
 #pip install streamlit
+
+#sudo apt-get update
+#sudo apt install python3-pip
+#pip3 install pandas 
+#sudo -h pip install -u pipenv 
+#pip3 install streamlit 
+
+
+#sudo apt install python3-venv
+
+
+############################################
+#On Azure --> My Mac Terminal 
+#sudo apt-get update
+#sudo apt install python3-pip
+# pip3 install streamlit
+#nano ~.bashrc
+#source ~.bashrc
+#~$ streamlit
+#$ pip install pandas
+#pip3 install jinja2
+#pip3 install jinja2 --upgrade
+#ls into repo 
+#streamlit run dash.py
+#copy external URL to see data visuals 
